36d386b52204e1968f88625ad65a427cd9679539
Bleizig <github@bleizig.net>
2022-12-03 20:59:21 +0100

    Jour 3 partie 2

---
+++ src/main/2022/Day3Part22022.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day3Part22022 {
+
+    public static void main(String[] args) {
+        List<SacsGroupe> sacsGroupes = lireSacADos();
+        int resultat = determinerSommePriorites(sacsGroupes);
+        System.out.println("Réponse=" + resultat);
+    }
+
+    private static int determinerSommePriorites(List<SacsGroupe> sacsGroupes) {
+        return sacsGroupes.stream().map(Day3Part22022::determinerPriorite).mapToInt(Integer::valueOf).sum();
+    }
+
+    private static int determinerPriorite(SacsGroupe sacsGroupe) {
+        for (char itemSac1 : sacsGroupe.sac1.toCharArray()) {
+            if (sacsGroupe.sac2.indexOf(itemSac1) != -1 && sacsGroupe.sac3.indexOf(itemSac1) != -1) {
+                return calculerScoreItem(itemSac1);
+            }
+        }
+        throw new RuntimeException("Pas d'item en triple");
+    }
+
+    private static int calculerScoreItem(char itemCompartiment) {
+        if (itemCompartiment >= 'a') {
+            return itemCompartiment - 'a' + 1;
+        } else {
+            return itemCompartiment - 'A' + 27;
+        }
+    }
+
+    private static List<SacsGroupe> lireSacADos() {
+        List<String> lignes = Util.lireFichier("entree.txt");
+
+        List<SacsGroupe> sacsGroupes = new ArrayList<>();
+        for (int i = 0; i < lignes.size(); i = i + 3) {
+            SacsGroupe sacsGroupe = new SacsGroupe(lignes.get(i), lignes.get(i + 1), lignes.get(i + 2));
+            sacsGroupes.add(sacsGroupe);
+        }
+        return sacsGroupes;
+    }
+
+
+    record SacsGroupe(String sac1, String sac2, String sac3) {
+    }
+}
